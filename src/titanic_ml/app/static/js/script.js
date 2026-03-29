559e50e55da86ffcadb11225c60360cd26a92b83
Kristiyan Bonev <k.s.bonev@gmail.com>
2026-03-29 20:49:18 +0300

    feat: add "Try with Jack/Rose" prefill buttons for quick demo testing
    - Add prefill buttons above the prediction form with movie character data
    - Jack Dawson: 20yo male, 3rd class, Southampton (should predict: did not survive)
    - Rose DeWitt Bukater: 17yo female, 1st class, Southampton (should predict: survived)
    - Update repo links in template to current repository URL

--- src/titanic_ml/app/static/js/script.js
+++ src/titanic_ml/app/static/js/script.js
@@ -8,6 +8,25 @@ function removePlaceholderOption(selectElement) {
     }
 }
 
+// Prefill form with movie character data
+function prefillCharacter(character) {
+    var characters = {
+        jack: { age: 20, gender: 'male', name_title: 'Mr', sibsp: '0', pclass: '3', embarked: 'S', cabin_multiple: '0' },
+        rose: { age: 17, gender: 'female', name_title: 'Miss', sibsp: '0', pclass: '1', embarked: 'S', cabin_multiple: '1' }
+    };
+
+    var data = characters[character];
+    if (!data) return;
+
+    document.querySelector('input[name="age"]').value = data.age;
+    document.querySelector('select[name="gender"]').value = data.gender;
+    document.querySelector('select[name="name_title"]').value = data.name_title;
+    document.querySelector('select[name="sibsp"]').value = data.sibsp;
+    document.querySelector('select[name="pclass"]').value = data.pclass;
+    document.querySelector('select[name="embarked"]').value = data.embarked;
+    document.querySelector('select[name="cabin_multiple"]').value = data.cabin_multiple;
+}
+
 // Smooth scroll to anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
